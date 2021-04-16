d932ff5ea9066dcf33bebc0b83c8d6a55247f1de
鲲鲲 <1607493862@qq.com>
2021-04-16 13:16:08 +0800

    Final

--- src/test.java
+++ src/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String [] args)
     {
-        System.out.println("Final  OK");
+        System.out.println("Final OK");
     }
 }
 //[remote "origin"]
