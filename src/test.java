4df2d60479069b02d2cf41e6f6e01482358abb1b
鲲鲲 <1607493862@qq.com>
2021-04-16 13:12:41 +0800

    Final

--- src/test.java
+++ src/test.java
@@ -1,6 +1,9 @@
 public class test {
     public static void main(String [] args)
     {
-        System.out.println("Final OK Back");
+        System.out.println("Final OK");
     }
 }
+//[remote "origin"]
+//	url = http://github.com/W-K-F/MXDX
+//	fetch = +refs/heads/*:refs/remotes/origin/*
